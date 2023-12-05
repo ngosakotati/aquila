ef23edebe638682a07726ff401886f7a05fc5011
ngosakotati <140051754+ngosakotati@users.noreply.github.com>
2023-12-05 09:21:50 +0200

    header and footer

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Footer template.
+ * 
+ * @package Aquila
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes();?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    <header>Header</header>
